49765d183bb917d53f953f35477e9ad470a6224e
Ferreira <ferreira.8390@gmail.com>
2023-04-28 01:26:13 -0400

    formtext

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+const contactForm = document.querySelector("#contact-form");
+const submitBtn = document.querySelector("#submit-btn");
+const nameInput = document.querySelector("#user_name");
+const emailInput = document.querySelector("#user_email");
+const messageInput = document.querySelector("#message");
+// const parrafo = document.querySelector("#text-hoverjs")     EJEMPLO DE OVERMOUSE;
+
+const publicKey = "bGvdSN4Lr2q4ifxPU";
+const serviceID = "service_1pwm3lz";
+const templateID = "template_qgxgbbr";
+
+// emailjs.init(publicKey);
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  submitBtn.innerText = "Un momento por favor..";
+
+  const inputFields = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  emailjs.send(serviceID, templateID, inputFields).then(
+    () => {
+      submitBtn.innerText = "Su mensaje fue enviado";
+      nameInput.value = "";
+      emailIinput.value = "";
+      messageInput.value = "";
+    },
+    (error) => {
+      console.log(error);
+
+      submitBtn.innerText = "Algo salió mal";
+    }
+  );
+});
+
+// EJEMPLO DE OVERMOUSE
+// parrafo.addEventListener("mouseover", (event) => {
+//   parrafo.innerHTML = "Hola qué tal";
+// });
+
+// parrafo.addEventListener("mouseleave", (event) => {
+//     parrafo.innerHTML = "Chaooo pescaooo"
+// })
